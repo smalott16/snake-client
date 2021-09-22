e8dffad1cf56ae6ea7117197c2750e620d57c0ee
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-22 22:14:14 +0000

    added arrow key movement functionality

--- input.js
+++ input.js
@@ -1,11 +1,34 @@
+//const {connect} = require('./client');
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  const connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
   const handleUserInput = (key) => {
+    if (key === 'w') {
+      console.log("Move: up")
+      connection.write("Move: up");
+    };
+
+    if (key === 's') {
+      console.log("Move: down")
+      connection.write("Move: down")
+    };
+
+    if (key === 'a') {
+      console.log("Move: left")
+      connection.write("Move: left")
+    };
+
+    if (key === 'd') {
+      console.log("Move: right")
+      connection.write("Move: right")
+    };
+    
     if (key === '\u0003') {
       process.exit();
     }
