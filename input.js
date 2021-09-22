7cfec332b3056f61374f2c17ffabbbcac4e16afd
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-22 21:39:57 +0000

    updated the play file to require the input file. Created the input file to handle the key inputs.

---
+++ input.js
@@ -0,0 +1,21 @@
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
